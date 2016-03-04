fbf81ff65194ccceb44526da1c5207b6a8ae8a29
jonathandturner <probata@hotmail.com>
2016-03-04 08:45:43 -0500

    Add Scope to API. Move todo to github

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,8 @@
 //! Rhai - embedded scripting for Rust
 
-
-// Todo (in no particular order):
-// * Doc some examples
-// *   Maintaining state
-// *   Overloading
-// *   How it works
-// * Vectors
-// * Tighten parser?
-// * Errors with positions?
-
 mod engine;
-
 mod fn_register;
 mod parser;
 
-pub use engine::Engine;
+pub use engine::{Engine, Scope};
 pub use fn_register::FnRegister;
